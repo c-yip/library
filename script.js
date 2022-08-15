ab7164d8b49c148cfd88f91fb333527d3927682d
Christian <christianyip92@gmail.com>
2022-08-15 10:18:43 -0700

    Add function that adds form input to book array.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let myLibrary = [];
 
 // object constructor for library
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = false;
+  this.read = read;
   this.info = function () {
     if (this.read === false) {
         return `The ${title} by ${author}, ${pages} pages, not read yet`;
@@ -26,15 +26,23 @@ Book.prototype.readFalse = function() {
 }
 
 // adds book to library
-function addBookToLibrary(title, author, pages) {
-  myLibrary.push(new Book(title, author, pages));
+// function addBookToLibrary(title, author, pages) {
+//   myLibrary.push(new Book(title, author, pages));
+//   console.log(myLibrary);
+// }
+
+// text to main content
+const mainContent = document.querySelector('.main-content');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let title = document.querySelector('#book-title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#page-numbers').value;
+  let read = document.querySelector('#read');
+  let value = read.options[read.selectedIndex].value;
+  console.log(value);
+  myLibrary.push(new Book(title, author, pages, value));
   console.log(myLibrary);
-}
-
-// test
-let bookOne = new Book('The Hobbit', 'Tolkien', 150);
-console.log(bookOne.info());
-bookOne.readTrue();
-console.log(bookOne.info());
-
-addBookToLibrary();
+});
